a530c8c069c314775355f07fff4a986bfe83c4f2
Mike-at-TRU <lebrunm231@mytru.ca>
2024-10-10 18:28:20 -0700

    added features to ModBlockClass

--- src/main/java/net/tru/trutermproject/block/ModBlocks.java
+++ src/main/java/net/tru/trutermproject/block/ModBlocks.java
@@ -1,13 +1,38 @@
 package net.tru.trutermproject.block;
 
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.tru.trutermproject.TRUTermProject;
+import net.tru.trutermproject.item.ModItems;
+
+import java.util.function.Supplier;
 
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(TRUTermProject.MOD_ID);
+/*
+how to create a block
+    public static final DeferredBlock<Block> BLOCK_NAME = registerBlock("block_name",
+            ()-> new Block(BlockBehaviour.Properties.of().strength(2f)));
+            .Properties.of() has a lot of things but I would say add strength, sound, and requiresCorrectToolForDrop
+*/
 
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block){
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+   //when you register a block you need to register an associated item so this is a method to do that
+private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block){
+    ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+}
 
     public static void register(IEventBus eventBus){
         BLOCKS.register(eventBus);
