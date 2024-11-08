c70fec384ab88c331318ef345bfe05112911d685
Mike-at-TRU <lebrunm231@mytru.ca>
2024-11-08 15:20:54 -0800

    Formating

--- src/main/java/net/tru/trutermproject/TRUTermProject.java
+++ src/main/java/net/tru/trutermproject/TRUTermProject.java
@@ -66,6 +66,7 @@ public class TRUTermProject {
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
+
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
