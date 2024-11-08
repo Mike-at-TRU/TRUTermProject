88fa026dacbb13e853ccef16ad59f06d96bb612b
Mike-at-TRU <lebrunm231@mytru.ca>
2024-11-08 12:21:20 -0800

    testing world gen

---
+++ src/main/java/net/tru/trutermproject/datagen/WorldGen/Ore/ModConfiguredFeatures.java
@@ -0,0 +1,45 @@
+package net.tru.trutermproject.datagen.WorldGen.Ore;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
+import net.tru.trutermproject.TRUTermProject;
+import net.tru.trutermproject.block.ModBlocks;
+import net.tru.trutermproject.util.ModTags;
+
+import java.util.List;
+
+public class ModConfiguredFeatures {
+    protected static ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_BERYL_ORE = createKey("overworld_beryl_ore");
+
+
+    public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
+        RuleTest stoneReplacable = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplacable = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+
+        List<OreConfiguration.TargetBlockState> examplOre =
+                List.of(OreConfiguration.target(stoneReplacable, ModBlocks.BERYL_ORE.get().defaultBlockState()),
+                        OreConfiguration.target(deepslateReplacable, ModBlocks.BERYL_DEEPSLATE_ORE.get().defaultBlockState()));
+
+        register(context, OVERWORLD_BERYL_ORE, Feature.ORE, new OreConfiguration(examplOre, 4));
+    }
+
+    private static ResourceKey<ConfiguredFeature<?, ?>> createKey(String name) {
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, ResourceLocation.fromNamespaceAndPath(TRUTermProject.MOD_ID, name));
+    }
+
+    private static <FC extends FeatureConfiguration, F extends Feature<FC>>
+    void register(BootstrapContext<ConfiguredFeature<?, ?>> context,
+                  ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC config) {
+
+        context.register(key, new ConfiguredFeature<>(feature, config));
+    }
+}
---
+++ src/main/java/net/tru/trutermproject/datagen/WorldGen/Ore/ModPlacedFeatures.java
@@ -0,0 +1,45 @@
+package net.tru.trutermproject.datagen.WorldGen.Ore;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+import net.tru.trutermproject.TRUTermProject;
+import org.joml.Vector2dc;
+
+import java.util.List;
+
+//stolen from https://www.youtube.com/watch?v=_7sc7INW0IY
+public class ModPlacedFeatures {
+    public static final ResourceKey<PlacedFeature> BERYL_ORE = createKey("beryl_ore");
+
+
+    public static void bootstrap(BootstrapContext<PlacedFeature> context) {
+        HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
+
+        Holder<ConfiguredFeature<?, ?>> holder =
+                configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_BERYL_ORE);
+
+        register(context, BERYL_ORE, holder, ModOrePlacement.commonOrePlacements(3,
+                HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(20))));
+    }
+
+
+    private static ResourceKey<PlacedFeature> createKey(String name) {
+        return ResourceKey.create(Registries.PLACED_FEATURE,
+                ResourceLocation.fromNamespaceAndPath(TRUTermProject.MOD_ID, name));
+    }
+
+    private static void register(BootstrapContext<PlacedFeature> context, ResourceKey<PlacedFeature> key
+            , Holder<ConfiguredFeature<?, ?>> feature, List<PlacementModifier> placementModifiers) {
+        context.register(key, new PlacedFeature(feature, placementModifiers));
+
+    }
+}
